9fac5870614056836f8b42b52c5496f78a7e99ca
AceRoqs <ace_roqs@yahoo.com>
2011-08-04 12:23:15 -0700

    Extract pow2 code.

--- Zobrist-Mersenne/zobrist-mersenne.cpp
+++ Zobrist-Mersenne/zobrist-mersenne.cpp
@@ -5,26 +5,7 @@
 #include <cassert>
 #include <cstdint>
 #include <iostream>
-
-// Specialized templates to compute powers of two at compile time
-// (template metaprogramming)
-template<int N>
-struct Pow2
-{
-    enum { value = 2 * Pow2<N - 1>::value };
-};
-
-template<>
-struct Pow2<0>
-{
-    enum { value = 1 };
-};
-
-template<int N>
-struct Pow2Minus1
-{
-    enum { value = Pow2<N - 1>::value - 1 + Pow2<N - 1>::value };
-};
+#include "pow2.h"
 
 // Parameters for MT19937
 // See Matsumoto for definition of parameters, and
