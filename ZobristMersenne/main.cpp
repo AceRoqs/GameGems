89eb3c4fb2a088fc26e2186092b97593c316e32f
Toby Jones <ace_roqs@yahoo.com>
2014-03-12 21:55:49 -0700

    Remove dashes in filenames.

---
+++ ZobristMersenne/main.cpp
@@ -0,0 +1,67 @@
+// Demonstration code for Zobrist Hash using the Mersenne Twister
+// Compiles under Visual Studio 2010 under Windows and gcc under Linux.
+// By Toby Jones
+
+#include <cstdint>
+#include <iostream>
+#include "mersenne.h"
+#include "zobrist.h"
+
+// Output the first 1024 generated numbers
+static void TestMT()
+{
+    std::cout << "Outputting the first 1024 generated numbers" << std::endl;
+
+    MersenneTwister rng;
+
+    for(int ii = 0; ii < 1024; ii++)
+    {
+        std::cout << rng.Rand() << std::endl;
+    }
+}
+
+// Demonstrate that a full hash calculation is the same as an incremental operation
+static void TestZH()
+{
+    ChessBoard chessBoard;
+
+    uint64_t initialZobristKey = chessBoard.CalculateZobristKey(WHITE);
+    std::cout << "Initial Zobrist Key: " << initialZobristKey << std::endl;
+
+    std::cout << "Moving white pawn from a2 to a4..." << std::endl;
+    uint64_t newIncrementalZobristKey = chessBoard.UpdateZobristKey(initialZobristKey,
+                                                                    W_PAWN,
+                                                                    8,
+                                                                    24);
+    std::cout << "New Zobrist Key (incremental): " << newIncrementalZobristKey << std::endl;
+
+    chessBoard.MovePiece(8, 24);
+    uint64_t newFullZobristKey = chessBoard.CalculateZobristKey(BLACK);
+    std::cout << "New Zobrist Key (full):        " << newFullZobristKey << std::endl;
+
+    if(newIncrementalZobristKey == newFullZobristKey)
+    {
+        std::cout << "Zobrist keys match." << std::endl;
+    }
+    else
+    {
+        std::cout << "Zobrist keys do _not_ match." << std::endl;
+    }
+}
+
+int main()
+{
+    std::cout.setf(std::ios::showbase);
+    std::cout.setf(std::ios::hex, std::ios::basefield);
+
+    std::cout << "-Testing Mersenne Twister-" << std::endl;
+    TestMT();
+
+    std::cout << std::endl;
+
+    std::cout << "-Testing Zobrist Hash-" << std::endl;
+    TestZH();
+
+    return 0;
+}
+
---
+++ ZobristMersenne/zobrist.h
@@ -0,0 +1,32 @@
+#ifndef ZOBRIST_H
+#define ZOBRIST_H
+
+// I only use the number of unique pieces, not the total number of pieces.  This
+// is because we want a key match on "equivilent" boards.  i.e. if we transposed
+// two same-colored rooks, the board is functionally the same.
+const int BOARD_SIZE = 8 * 8;   // size of chess board
+const int NUM_PIECES = 6;       // rook, knight, bishop, king, queen, pawn
+const int NUM_COLORS = 2;       // white, black
+
+enum eChessPiece { B_ROOK, B_KNIGHT, B_BISHOP, B_KING, B_QUEEN, B_PAWN,
+                   W_ROOK, W_KNIGHT, W_BISHOP, W_KING, W_QUEEN, W_PAWN,
+                   EMPTY };
+enum eColor { BLACK, WHITE };
+
+class ChessBoard
+{
+    uint64_t m_aZobristTable[BOARD_SIZE][NUM_PIECES][NUM_COLORS];
+    eChessPiece m_aBoard[BOARD_SIZE];
+
+    void InitializeZobristTable();
+    void PopulateChessBoard();
+
+public:
+    ChessBoard();
+    uint64_t CalculateZobristKey(eColor sideToMove) const;
+    uint64_t UpdateZobristKey(uint64_t oldKey, eChessPiece piece, int oldPos, int newPos) const;
+    void MovePiece(int oldPos, int newPos);
+};
+
+#endif
+
